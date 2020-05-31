30ea7cdacaa7ded718073ac5fa77d0a8caeb7d14
BrightonDube <BrightonDube@users.noreply.github.com>
2020-05-31 15:10:06 +0200

    fix onclick bug

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,28 +1,39 @@
 //jshint esversion: 9
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Form, Button, Col } from "react-bootstrap";
 
-const InputArea = ({onClicked}) => {
-    const [state, setState] = useState("");
-    const updateList = (event)=> {
-        setState(event.target.value);
-    };
-    return(
-        <Form onSubmit={(event)=>{onClicked(state, setState, event)}}>
-          <Form.Row>
-              <Form.Control placeholder="To-do Item"
-                  name="listItem"
-                  type="text"
-                  value={state}
-                  onChange={updateList}
-                  className="input"                    
-              />
-              <Button as={Col} onClick={()=>{onClicked(state, setState)}} className="submit">
-                  Add Item
-              </Button>
-          </Form.Row>
-      </Form>
-    );
-}
+const InputArea = ({ onClicked }) => {
+  const [state, setState] = useState("");
+  const updateList = (event) => {
+    setState(event.target.value);
+  };
+  return (
+    <Form
+      onSubmit={(event) => {
+        onClicked(state, setState, event);
+      }}
+    >
+      <Form.Row>
+        <Form.Control
+          placeholder="To-do Item"
+          name="listItem"
+          type="text"
+          value={state}
+          onChange={updateList}
+          className="input"
+        />
+        <Button
+          as={Col}
+          onClick={(event) => {
+            onClicked(state, setState, event);
+          }}
+          className="submit"
+        >
+          Add Item
+        </Button>
+      </Form.Row>
+    </Form>
+  );
+};
 
 export default InputArea;
